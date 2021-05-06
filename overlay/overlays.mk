408108eeef8a4dcc260fcf80933b7f9c40409627
timocapa <timo.capa@gmail.com>
2021-05-06 22:35:48 +0200

    pa: Add Manrope font
    	* Based on reverting 5748954ca55b924527ce09cb1500102b9d6b745e
    	* Italic fonts are from an open PR on GitHub
    	* Moved fonts_customization.xml from pixel repo back to vendor/pa
    
    Change-Id: I738004d39673babe370ae43ee76fb8a337af35e5

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -36,6 +36,7 @@ PRODUCT_PACKAGES += \
     AOSPATelephonyOverlay \
     AOSPAThemePickerOverlay \
     FontGoogleSansOverlay \
+    FontManropeOverlay \
     ParanoidDozeOverlay \
     ParanoidHubOverlay \
     ParanoidLauncherOverlay
